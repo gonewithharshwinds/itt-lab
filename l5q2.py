bfab9d5585840ce5a6bb893d62b98ec396937ffe
Harsh ADV) <70069185+gonewithharshwinds@users.noreply.github.com>
2022-03-24 08:59:06 +0530

    Add files via upload

---
+++ l5q2.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+
+def transpose(matrix):
+    rows = len(matrix)
+    columns = len(matrix[0])
+
+    matrix_T = []
+    for j in range(columns):
+        row = []
+        for i in range(rows):
+           row.append(matrix[i][j])
+        matrix_T.append(row)
+
+    return matrix_T
+
+print("Enter dimension of matrix")
